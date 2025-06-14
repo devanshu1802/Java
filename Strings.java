fc1157c647285a5b5081e559e66065b8954b87c9
Devanshu <devanshusingh530@gmail.com>
2025-06-14 22:12:53 +0530

    str builder functions

--- Strings.java
+++ Strings.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class Strings {
     public static void main(String arg[]){
 
@@ -8,12 +8,11 @@ public class Strings {
 
         System.out.println(fullname.length());
 
-        // For printing a char at a index
+    // For printing a char at a index
         for(int i=0; i<fullname.length(); i++){
             System.out.println(fullname.charAt(i));
         }
-
-        // Comparing 2 Strings
+    // Comparing 2 Strings
         // s1 > s2 : +ve value
         // s1 = s2 : 0
         // s1 < s2 : -ve value
@@ -24,9 +23,32 @@ public class Strings {
         else
             System.out.println("They are not equal");
 
-        // Substring
+    // Substring
         // substring(begin index, end index)
         System.out.println(firstname.substring(3));
+
+// ------------------String builder----------------
+
+        StringBuilder sb = new StringBuilder("sinsinwar");
+        System.out.println(sb);
+
+        System.out.println(sb.charAt(0));
+
+    // To replace a character at a given index
+        sb.setCharAt(0,'S');
+        System.out.println(sb);
+
+    // To insert something in a string
+        sb.insert(0, "Devdat ");
+        System.out.println(sb);
+
+    // To delete a char or a substring
+        sb.delete(0, 6);
+        System.out.println(sb);
+
+    // To append something at the end
+        sb.append(" Don");
+        System.out.println(sb);
     }
     
 }
