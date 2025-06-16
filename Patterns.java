03a22e73e379585190d5d1fed15eaeb58905867c
Devanshu <devanshusingh530@gmail.com>
2025-06-16 20:57:28 +0530

    basic patterns of shapes

--- Patterns.java
+++ Patterns.java
@@ -2,7 +2,7 @@ import java.util.*;
 @SuppressWarnings("resource")
 
 // Shape Patterns
-public class Class_2 {
+public class Patterns {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);                        
         int n = sc.nextInt();                                       
