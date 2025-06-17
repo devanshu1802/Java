01a7a3afa70d55184f778b8942a35a2f2c80596b
Devanshu <devanshusingh530@gmail.com>
2025-06-17 20:32:51 +0530

    factorial code

--- Recursion.java
+++ Recursion.java
@@ -1,6 +1,6 @@
 public class Recursion{
     // To find the sum of first n natural numbers
-    
+
     public static void printsum(int i , int n ,int sum){
         if(i == n){
             sum += i;
@@ -11,8 +11,24 @@ public class Recursion{
         printsum(i+1, n, sum);
     }
 
+    // To find the factorial of a given number
+
+    public static int calfactorial(int n){
+        if(n==1 || n==0){
+            return 1;
+        }
+        else{
+
+            return n* calfactorial(n-1);
+        }
+    }
+
+
     public static void main(String arg[]){
         printsum(1, 7, 0);
+
+        int factorial =calfactorial(5);
+        System.out.println("The factorial of the given number is "+factorial);
 
     }
 }
